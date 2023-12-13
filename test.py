c8fa70b4aac4f633e0feddc080b2a389767d7e70
arsbul <ars.bulygin@yandex.ru>
2023-12-13 10:35:21 +0300

    bugs

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
-def first(line="YOOOOOOX"):
-    x_index = line.find("X")
-    y_index = line.find("Y")
-    if x_index < 0 or y_index < 0:
-        return None
+import math
+
+
+def first(line: list):
+    if "X" not in line or "Y" not in line:
+        return 0
+    x_index = line.index("X")
+    y_index = line.index("Y")
     return abs(y_index - x_index)
+
 
 def second(data=[1, 3, -1, -4]):
     d = 0
     indexes = (0, 0)
     started_type = None
     for i in range(len(data)):
-        if i < len(data):
+        if i + 1 < len(data):
             num = data[i]
             next_num = data[i + 1]
             if num < next_num:
@@ -26,5 +30,39 @@ def second(data=[1, 3, -1, -4]):
                 else:
                     d += 1
 
+    return d
 
-print(first())
+
+def third(nums: list, target: int):
+    for i in nums:
+        s_num = target - i
+        if s_num in nums:
+            return nums.index(i), nums.index(s_num)
+
+
+print(third([1, 2, 3, 4, 5], 7))
+
+
+def fourth(num):
+    string_num_reversed = list(str(num))
+    string_num_reversed.reverse()
+    if list(str(num)) == string_num_reversed:
+        return True
+    return False
+
+
+def five(list1: list, list2: list):
+    out_list = list1 + list2
+    out_list.sort()
+    return out_list
+
+
+from math import ceil, sqrt, floor
+
+
+def six(x: int):
+    x = sqrt(x)
+    if x % int(x) >= 0.5:
+        return ceil(x)
+    else:
+        return floor(x)
